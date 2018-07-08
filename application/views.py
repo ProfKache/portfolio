db46022460adfe13e4c5ec359bff36c06bde3cb8
Salim J Kachemela <sakachemela@gmail.com>
2018-07-08 18:57:20 +0300

    Modify views to add error handler view

--- application/views.py
+++ application/views.py
@@ -5,3 +5,8 @@ from application import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
