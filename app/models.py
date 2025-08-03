6f6dc8b7354156bbfd33338e011a7a65df38fd36
Salim Kachemela <sakachemela@gmail.com>
2025-08-03 14:58:11 +0300

    Add model for adding technologies

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Technology(models.Model):
+    """
+    A model that store information about a tech stack e.g
+    HTML, CSS, JavaScript, Django etc.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Technologies"
+
+    def __str__(self):
+        return self.name
