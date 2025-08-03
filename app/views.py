8969bd3b1e6924e2faf7251b6d04cf2265cba8a6
Salim Kachemela <sakachemela@gmail.com>
2025-08-03 22:22:41 +0300

    Update index view to render tools section from the database

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from app.models import TechCategory
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html")
+    tech_categories = TechCategory.objects.prefetch_related("technologies").order_by("id").all()
+
+    context = {"tech_categories": tech_categories}
+    return render(request, "index.html", context)
