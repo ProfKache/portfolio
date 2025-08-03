8407e2da9789ef70e77123d5b5845f5a5cebd81f
Salim Kachemela <sakachemela@gmail.com>
2025-08-03 22:22:02 +0300

    Add new model for managing tech categories

--- app/models.py
+++ app/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 
+class TechCategory(models.Model):
+    """
+    A model that store information about a tech stack category e.g
+    Frontend, Backend, Database etc.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Tech Categories"
+
+    def __str__(self):
+        return self.name
+
+
 class Technology(models.Model):
     """
     A model that store information about a tech stack e.g
@@ -8,6 +23,7 @@ class Technology(models.Model):
     """
 
     name = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey(TechCategory, on_delete=models.CASCADE, related_name="technologies")
 
     class Meta:
         verbose_name_plural = "Technologies"
