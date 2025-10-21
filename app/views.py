a895199749e6642b665c68496396d5488b5ff551
Salim Kachemela <sakachemela@gmail.com>
2025-10-21 17:24:16 +0300

    Add Testmonial Section loading from dynamic views

--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from app.models import Service, TechCategory
+from app.models import Service, TechCategory, Testimonial
 
 
 def index(request: HttpRequest) -> HttpResponse:
     tech_categories = TechCategory.objects.prefetch_related("technologies").order_by("id").filter(is_active=True)
     services = Service.objects.filter(is_active=True).order_by("id")
+    testmonials = Testimonial.objects.filter(is_active=True).order_by("id")
 
-    context = {"tech_categories": tech_categories, "services": services}
+    context = {
+        "tech_categories": tech_categories,
+        "services": services,
+        "testmonials": testmonials,
+    }
     return render(request, "index.html", context)
