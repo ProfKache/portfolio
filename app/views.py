73ab6c969f6c18fd0187b4fe41e9bd76c7cc407d
Salim Kachemela <sakachemela@gmail.com>
2025-10-21 16:20:31 +0300

    Add new queryset for fetching all active services

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from app.models import TechCategory
+from app.models import Service, TechCategory
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    tech_categories = TechCategory.objects.prefetch_related("technologies").order_by("id").all()
+    tech_categories = TechCategory.objects.prefetch_related("technologies").order_by("id").filter(is_active=True)
+    services = Service.objects.filter(is_active=True).order_by("id")
 
-    context = {"tech_categories": tech_categories}
+    context = {"tech_categories": tech_categories, "services": services}
     return render(request, "index.html", context)
