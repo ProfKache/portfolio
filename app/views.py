b2338220d6cd8c757cfa9da7758395ef0cb7fc52
Salim Kachemela <sakachemela@gmail.com>
2025-10-22 13:55:27 +0300

    Add queryset for fetching all the projects I have worked with

--- app/views.py
+++ app/views.py
@@ -1,17 +1,19 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from app.models import Service, TechCategory, Testimonial
+from app.models import Project, Service, TechCategory, Testimonial
 
 
 def index(request: HttpRequest) -> HttpResponse:
     tech_categories = TechCategory.objects.prefetch_related("technologies").order_by("id").filter(is_active=True)
     services = Service.objects.filter(is_active=True).order_by("id")
     testmonials = Testimonial.objects.filter(is_active=True).order_by("id")
+    projects = Project.objects.filter(is_active=True).order_by("id")
 
     context = {
         "tech_categories": tech_categories,
         "services": services,
         "testmonials": testmonials,
+        "projects": projects,
     }
     return render(request, "index.html", context)
