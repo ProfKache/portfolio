20fd9df4e7846019753e36045c28fae26692cef9
Salim Kachemela <sakachemela@gmail.com>
2025-10-22 17:17:13 +0300

    Update url to use slugfield instead of primary key for security

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path(
+        "project/<slug:slug>",
+        views.project_details,
+        name="project_details",
+    ),
 ]
