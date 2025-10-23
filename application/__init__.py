326ccb8f32d4abf65a890fbb0ab0cb8bb4b42354
Salim Kachemela <sakachemela@gmail.com>
2025-10-23 23:32:48 +0300

    Remove flask directory in new main branch

--- application/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_bootstrap import Bootstrap
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-
-import application.views
--- application/static/README.md
+++
@@ -1,2 +0,0 @@
-# portfolio-salim
-My portfolio specifically for advertising my skills in programming :) and also for fun!!!
